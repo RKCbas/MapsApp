2ce07bb0a2b5ab79b10183966ad038c6598a7123
RKCbas <sebashp2015@hotmail.com>
2025-05-28 17:49:01 -0600

    # Class 1 ✨Feat: Basic configuration of the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'fullscreen',
+    title: 'FullScreen Map',
+    loadComponent: () => import('./pages/fullscreen-map-page/fullscreen-map-page.component')
+  },
+  {
+    path: 'markers',
+    title: 'Markers',
+    loadComponent: () => import('./pages/fullscreen-map-page/fullscreen-map-page.component')
+  },
+  {
+    path: 'houses',
+    title: 'Available houses',
+    loadComponent: () => import('./pages/houses-page/houses-page.component')
+  },
+  {
+    path: '**',
+    redirectTo: 'fullscreen'
+  },
+
+
+];
