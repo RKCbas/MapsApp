c6e30fb5de51a5e44a4cd4b81d6707a03e4ba402
RKCbas <sebashp2015@hotmail.com>
2025-05-29 16:33:10 -0600

    # Class 2 Titulo en el navbar ✨Feat: Adapted the navbar to display the current page and created load the items in the navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,9 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { routes } from '../../../app.routes';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { filter, map } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    AsyncPipe,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './navbar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+
+  router = inject(Router)
+
+  routes = routes.map( route => ({
+    path: route.path,
+    title: `${route.title ?? 'Mapas en Angular'}`,
+  })).filter( route => route.path !== '**')
+
+  pageTitle$ = this.router.events.pipe(
+    filter( event => event instanceof NavigationEnd ),
+    map( event => event.url ),
+    map( url => this.routes.find(route => `/${route.path}` === url)?.title ?? 'Mapas' )
+  )
+
+
+}
