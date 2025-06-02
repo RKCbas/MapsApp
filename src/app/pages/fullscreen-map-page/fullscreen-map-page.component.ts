5dd02968e814eba92d7e4ca5f22091d2606a9119
RKCbas <sebashp2015@hotmail.com>
2025-06-02 17:05:44 -0600

    # Class 6 Controlar mapa - zoom ✨Feat: Configured the zoom control of the map of fullScreen page

--- src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
+++ src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
@@ -1,12 +1,15 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, viewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, effect, ElementRef, signal, viewChild } from '@angular/core';
 import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 import { environment } from '../../../environments/environment';
+import { DecimalPipe } from '@angular/common';
 
 mapboxgl.accessToken = environment.mapboxKey;
 
 @Component({
   selector: 'app-fullscreen-map-page',
-  imports: [],
+  imports: [
+    DecimalPipe
+  ],
   templateUrl: './fullscreen-map-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   styles: `
@@ -14,17 +17,38 @@ mapboxgl.accessToken = environment.mapboxKey;
       width: 100vw;
       height: calc(100vh - 64px);
     }
+    #controls {
+      background-color: white;
+      padding: 10px;
+      border-radius: 5px;
+      position: fixed;
+      bottom: 25px;
+      right: 20px;
+      z-index: 9999;
+      box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+      border: 1px solid #e2e8f0;
+      width: 250px;
+    }
   `
 })
 export default class FullscreenMapPageComponent implements AfterViewInit {
 
   divElement = viewChild<ElementRef>('map')
+  map = signal<mapboxgl.Map | null>(null)
+  zoom = signal(14);
+
+  zoomEffect = effect(()=>{
+    if( !this.map() ) return
+
+    this.map()?.zoomTo(this.zoom())
+
+  })
 
   async ngAfterViewInit() {
 
-    if ( !this.divElement()?.nativeElement  ) return;
+    if (!this.divElement()?.nativeElement) return;
 
-    await new Promise((resolve)=> setTimeout(resolve, 80));
+    await new Promise((resolve) => setTimeout(resolve, 80));
 
     const element = this.divElement()?.nativeElement;
     console.log(element);
@@ -33,12 +57,21 @@ export default class FullscreenMapPageComponent implements AfterViewInit {
       container: element, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: this.zoom(), // starting zoom
     });
 
-
-
+    this.mapListeners(map)
   };
+
+  mapListeners( map:mapboxgl.Map ){
+
+    map.on('zoomend', (event) =>{
+      const newZoom = event.target.getZoom();
+      this.zoom.set(newZoom);
+    })
+
+    this.map.set(map);
+  }
 
 
 
